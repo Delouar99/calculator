b6677603655c1a4a9f59f9e8b93b99d3c5a6df8d
delouar <delouar1998@gmail.com>
2022-01-30 13:04:24 +0600

    upload all file

---
+++ app.js
@@ -0,0 +1,30 @@
+//get element....
+
+let cal_data = [];
+
+let valGat = (val) => {
+    
+ cal_data.push(val);
+
+    document.querySelector('.top-monitor').innerHTML = cal_data.join(' ')
+    document.querySelector('.main-monitor').innerHTML = 0 ;
+
+}
+
+let allclear = () => {
+    cal_data = [];
+    document.querySelector('.top-monitor').innerHTML = 0; 
+    document.querySelector('.main-monitor').innerHTML = ' ' ;
+}
+
+let back = () =>{
+cal_data.pop();
+document.querySelector('.top-monitor').innerHTML = cal_data.join(' ')
+
+}
+
+
+let FinalRes = () =>{
+    let cal_string = cal_data.join('');
+    document.querySelector('.main-monitor').innerHTML = eval(cal_string);
+}
